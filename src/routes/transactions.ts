ee843614ef687dba5ca67b52d7eeac2d33678642
viniciuskj <vinicius.koe@puccampinas.edu.br>
2025-05-08 16:19:38 -0300

    feat: add middleware sessionId

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { z } from 'zod'
-import crypto from 'node:crypto'
+import crypto, { randomUUID } from 'node:crypto'
+import { checkSessionIdExists } from '../middleware/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   // GET - POST - PUT - PATCH - DELETE -> Podemos chamr a funçao de cada para oo app
@@ -15,57 +16,112 @@ export async function transactionsRoutes(app: FastifyInstance) {
     const { title, amount, type } = createTransactionBodySchema.parse(
       request.body,
     )
+
+    // Recebo a sessionId por meio do cookie
+    let sessionId = request.cookies.sessionId
+
+    // Se nao tiver uma sessionId, adiciono uma nova com randomUUID
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        // Todas as rotas podem acessar esse cookie
+        path: '/',
+        // Quanto em segundos esse cookie vai durar
+        maxAge: 60 * 60 * 24 * 7, // 7 Dias
+      })
+    }
     // Inseri na tabela transactions, e verifica o tipo do amount
     await knex('transactions').insert({
       id: crypto.randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      // Insiro a sessionId existente ou criaada
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
   })
 
-  app.get('/', async (request, reply) => {
-    // Seleciono tudo da tabela transactions
-    const transactions = await knex.select('*').from('transactions')
-    // Envio como objeto caso no futuro queira adicionar mais sessoes
-    return reply.status(200).send({
-      transactions,
-    })
-  })
+  app.get(
+    '/',
+    // Quando o usuario acessar a rota raiz, antes de executar a funcao handler, ele passa por uma checkagem antes com o prehandler(middleware)
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
+      // Seleciono tudo da tabela transactions que possui sessionid
+      const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select()
+      // Envio como objeto caso no futuro queira adicionar mais sessoes
+      return reply.status(200).send({
+        transactions,
+      })
+    },
+  )
 
-  app.get('/:id', async (request, reply) => {
-    // Valido o uuid para conseguir ele como parametro
-    const getTransactionByIdSchema = z.object({
-      id: z.string().uuid(),
-    })
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const { sessionId } = request.cookies
 
-    const { id } = getTransactionByIdSchema.parse(request.params)
-    // Seleciono o id especifico
-    const transaction = await knex('transactions').where('id', id).first()
+      // Valido o uuid para conseguir ele como parametro
+      const getTransactionByIdSchema = z.object({
+        id: z.string().uuid(),
+      })
 
-    return reply.status(201).send(transaction)
-  })
+      const { id } = getTransactionByIdSchema.parse(request.params)
+      // Seleciono o id especifico
+      const transaction = await knex('transactions')
+        .where({
+          session_id: sessionId,
+          id,
+        })
+        .first()
 
-  app.get('/summary', async () => {
-    const summary = await knex('transactions')
-      // Soma tudo que esta em amount, "as" para dar nome a essa soma
-      .sum('amount', { as: 'amount' })
-      .first()
+      return reply.status(201).send(transaction)
+    },
+  )
 
-    return { summary }
-  })
+  app.get(
+    '/summary',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies
 
-  app.delete('/:id', async (request, reply) => {
-    // Valido o id para coseguir passar como parametro
-    const deleteTransactionByIdSchema = z.object({
-      id: z.string(),
-    })
+      const summary = await knex('transactions')
+        .where({ session_id: sessionId })
+        // Soma tudo que esta em amount, "as" para dar nome a essa soma
+        .sum('amount', { as: 'amount' })
+        .first()
 
-    const { id } = deleteTransactionByIdSchema.parse(request.params)
-    // Deletar o id especifico
-    await knex('transactions').where('id', id).del()
+      return { summary }
+    },
+  )
 
-    return reply.status(204).send()
-  })
+  app.delete(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      // Valido o id para coseguir passar como parametro
+      const deleteTransactionByIdSchema = z.object({
+        id: z.string(),
+      })
+
+      const { id } = deleteTransactionByIdSchema.parse(request.params)
+      // Deletar o id especifico
+      await knex('transactions').where('id', id).del()
+
+      return reply.status(204).send()
+    },
+  )
 }
